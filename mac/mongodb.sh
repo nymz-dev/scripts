f5f71ee178631e28ed87cd3228a616ba120acffd
jewishmoses <work@xmoses.com>
2022-05-07 17:19:17 +0300

    minor changes

--- mac/mongodb.sh
+++ mac/mongodb.sh
@@ -1,9 +1,8 @@
-# requires Xcode Command Line Tools and Homebrew
+#!/bin/bash
+# Requirements: Xcode Command Line Tools and Homebrew
 
 xcode-select --install
 brew tap mongodb/brew
 brew update
 brew install mongodb-community
 brew services start mongodb-community
-
-# To stop brew services stop mongodb-community
